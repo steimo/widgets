ebf2b2b0f423547c5bbbc0e7f2e5c8f21a3ccf3a
steimo <vladyslav.kolomiets@protonmail.com>
2023-08-15 10:26:29 +0300

    Add widgets_controller and widget_ratings_controller; alter routes.rb.

---
+++ app/controllers/widgets_controller.rb
@@ -0,0 +1,15 @@
+class WidgetsController < ApplicationController
+
+  def index
+    @widgets = [
+      OpenStruct.new(id: 1, name: "Stembolt"),
+      OpenStruct.new(id: 2, name: "Flux Capacitor"),
+    ]
+  end
+
+  def show
+    @widget = OpenStruct.new(id: params[:id],
+                             manufacturer_id: rand(100),
+                             name: "Widget #{params[:id]}")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,25 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "widgets#index"
+
+  resources :widgets, only: [:show, :index]
+  resources :widget_ratings, only: [:create]
+
+  namespace :customer_service do
+    resources :widgets, only: [:show, :update, :destroy]
+  end
+
+  ####
+  # Custom routes start here
+  #
+  # For each new custom route:
+  #
+  # * Be sure you have the canonical route declared above
+  # * Add the new custom route below the existing ones
+  # * Document why it's needed
+  # * Explain anything else non-standard
+
+  # Used in podcast ads for the 'amazing' campaign
+  get "/amazing", to: redirect("/widgets")
 end
