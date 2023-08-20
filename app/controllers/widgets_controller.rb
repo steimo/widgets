b4f95109e07ed728444eeeb3930202316e6f80ad
steimo <vladyslav.kolomiets@protonmail.com>
2023-08-20 11:59:40 +0300

    Update widgets views.

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -8,8 +8,17 @@ class WidgetsController < ApplicationController
   end
 
   def show
+    manufacturer = OpenStruct.new(
+      id: rand(100),
+      name: "Sector 7G",
+      address: OpenStruct.new(
+        id: rand(100),
+        country: "UK"
+      )
+    )
     @widget = OpenStruct.new(id: params[:id],
-                             manufacturer_id: rand(100),
+                             manufacturer_id: manufacturer.id,
+                             manufacturer: manufacturer,
                              name: "Widget #{params[:id]}")
   end
 end
