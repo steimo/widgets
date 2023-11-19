b5f26758029d45220dcc306a97e4d3477f7d504c
steimo <vladyslav.kolomiets@protonmail.com>
2023-11-19 14:43:31 +0200

    Add widget and manufacturer active records; add user_shipping_estimate active model.

---
+++ app/models/widget.rb
@@ -0,0 +1,20 @@
+class Widget < ApplicationRecord
+  belongs_to :manufacturer
+
+  def self.fresh
+    self.where(status: "fresh")
+  end
+
+  def user_facing_identifier
+    id_as_string = self.id.to_s
+    if id_as_string.length < 3
+      return id_as_string
+    end
+
+    "%{first}.%{last_two}" % {
+      first: id_as_string[0..-3],
+      last_two: id_as_string[-2..-1]
+    }
+  end
+end
+
