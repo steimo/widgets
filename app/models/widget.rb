da5c8321d7ebb2999767ce64a4d2f85360598fc7
steimo <vladyslav.kolomiets@protonmail.com>
2023-12-07 22:39:46 +0200

    Update migrations; add db scripts; update models.

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,5 +1,8 @@
 class Widget < ApplicationRecord
   belongs_to :manufacturer
+  belongs_to :widget_status
+  validates :price_cents,
+            numericality: { less_than_or_equal_to: 10_000_00 }
 
   def self.fresh
     self.where(status: "fresh")
