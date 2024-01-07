b87e75a98d0a6ba46bee0c8adb91ea0f7355a7d1
steimo <vladyslav.kolomiets@protonmail.com>
2024-01-07 20:54:57 +0200

    Update widget validations; update mailers; update tests.

--- test/services/widget_creator_test.rb
+++ test/services/widget_creator_test.rb
@@ -57,5 +57,90 @@ class WidgetCreatorTest < ActiveSupport::TestCase
     assert_equal "finance@example.com", mail_message["to"].to_s
     assert_match /Stembolt/, mail_message.text_part.to_s
   end
+
+  test "name, price, and manufacturer are required" do
+    result = @widget_creator.create_widget(Widget.new)
+
+    refute result.created?
+
+    widget = result.widget
+    assert widget.invalid?
+
+    assert widget.errors[:name].any? { |message|
+      message =~ /can't be blank/i
+    }, widget.errors.full_messages_for(:name)
+
+    assert widget.errors[:price_cents].any? { |message|
+      message =~ /is not a number/i
+    }, widget.errors.full_messages_for(:price_cents)
+
+    assert widget.errors[:manufacturer].any? { |message|
+      message =~ /must exist/i
+    }, widget.errors.full_messages_for(:manufacturer)
+  end
+
+  test "price cannot be 0" do
+    result = @widget_creator.create_widget(Widget.new(
+      name: "Stembolt",
+      price_cents: 0,
+      manufacturer_id: @manufacturer.id
+    ))
+
+    refute result.created?
+
+    assert result.widget.errors[:price_cents].any? { |message|
+      message =~ /greater than 0/i
+    }, result.widget.errors.full_messages_for(:price_cents)
+  end
+
+  test "price cannot be more than 10,000" do
+    result = @widget_creator.create_widget(Widget.new(
+      name: "Stembolt",
+      price_cents: 10_000_01,
+      manufacturer_id: @manufacturer.id
+    ))
+    refute result.created?
+
+    assert result.widget.errors[:price_cents].any? { |message|
+      message =~ /less than or equal to 1000000/i
+    }, result.widget.errors.full_messages_for(:price_cents)
+  end
+
+  test "legacy manufacturers cannot have a price under $100" do
+    legacy_manufacturer = FactoryBot.create(:manufacturer,
+                                            created_at: DateTime.new(2010, 1, 1) - 1.day)
+
+    result = @widget_creator.create_widget(Widget.new(
+      name: "Stembolt",
+      price_cents: 99_00,
+      manufacturer_id: legacy_manufacturer.id
+    ))
+
+    refute result.created?
+
+    assert result.widget.errors[:price_cents].any? { |message|
+      message =~ /< \$100.*legacy/i
+    }, result.widget.errors.full_messages_for(:price_cents)
+  end
+
+  test "email admin staff for widgets on new manufacturers " do
+    new_manufacturer = FactoryBot.create(:manufacturer,
+                                         name: "Cyberdine Systems",
+                                         created_at: 59.days.ago)
+
+    result = @widget_creator.create_widget(Widget.new(
+      name: "Stembolt",
+      price_cents: 99_00,
+      manufacturer_id: new_manufacturer.id
+    ))
+
+    assert result.created?
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    mail_message = ActionMailer::Base.deliveries.first
+    assert_equal "admin@example.com", mail_message["to"].to_s
+    assert_match /Stembolt/, mail_message.text_part.to_s
+    assert_match /Cyberdine Systems/, mail_message.text_part.to_s
+  end
 end
 
--- test/system/rate_widget_test.rb
+++ test/system/rate_widget_test.rb
@@ -2,7 +2,8 @@ require "application_system_test_case"
 
 class RateWidgetsTest < BrowserSystemTestCase
   test "rating a widget shows our rating inline" do
-    visit widget_path(1234)
+    widget = FactoryBot.create(:widget)
+    visit widget_path(widget)
 
     click_on "2"
     assert_selector "[data-rating-present]",
